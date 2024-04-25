99d5a3e45ade56ef2dc5a38109c3fa7e2e854c08
BIMAL CHOOUDHURY <57108860+bimal99370@users.noreply.github.com>
2024-04-25 17:35:30 +0530

    Add files via upload

---
+++ weather data.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime, date
+
+
+df = pd.read_csv("greek_weather_data.csv")
+print(df.head(10))
+
